7e17ac544acf951c56f0235611b5556ef11bfb23
Oxana <mail.suvorova.o@gmail.com>
2024-07-19 23:41:39 -0400

    JAT-37 Calculate Other Taxes. Moved calculation to the Apex controller

--- force-app/main/default/lwc/takeHomePayEstimator/takeHomePayEstimator.js
+++ force-app/main/default/lwc/takeHomePayEstimator/takeHomePayEstimator.js
@@ -1,15 +1,28 @@
 import { LightningElement, track, wire } from 'lwc';
 import getTaxDeduction from '@salesforce/apex/TakeHomePayEstimatorController.getTaxDeduction';
-import getTaxBrackets from '@salesforce/apex/TakeHomePayEstimatorController.getTaxBrackets';
+import getTaxesAndPayChecks from '@salesforce/apex/TakeHomePayEstimatorController.getTaxesAndPayChecks';
 
 export default class TakeHomePayEstimator extends LightningElement {
     annualIncome = 100000;
     filingStatus = 'Single';
     standardDeduction = 0;
-    taxableIncome;
     federalTaxValue = 0;
+    socialSecurity = 0;
+    medicare = 0;
 
     @track error;
+
+    get taxableIncome() {
+        return this.annualIncome - this.standardDeduction;
+    }
+
+    get annualPaycheck() {
+        return this.annualIncome - this.federalTaxValue - this.socialSecurity - this.medicare;
+    }
+
+    get monthlyPaycheck() {
+        return this.annualPaycheck / 12;
+    }
 
     filingStatusOptions = [
         { label: 'Single', value: 'Single' },
@@ -17,9 +30,6 @@ export default class TakeHomePayEstimator extends LightningElement {
         { label: 'Married, filing separately', value: 'MarriedFSeparately' },
         { label: 'Head of household', value: 'HeadOfHousehold' }
     ];
-
-    @wire(getTaxBrackets, { filingStatus: '$filingStatus' } )
-    taxBrackets;
 
     annualIncomeChange(event) {
         this.annualIncome = event.target.value;
@@ -33,19 +43,33 @@ export default class TakeHomePayEstimator extends LightningElement {
     setStandardDeduction({ error, data }) {
         if (data) {
             this.standardDeduction = data;
-            this.taxableIncome = this.annualIncome - this.standardDeduction;
+            this.error = undefined;
         } else if (error) {
             this.error = error;
         }
         
     };
 
-    handleEstimateClick() {
-        for (const bracket of this.taxBrackets.data) {
-            if (this.taxableIncome > bracket.Min_Bracket__c) {
-                this.federalTaxValue = (this.taxableIncome - bracket.Min_Bracket__c) * bracket.Tax_Rate__c + bracket.Tax_Value__c;
-                break;
-            }
+    async handleEstimateClick() {
+        // Create the object with parameters with
+        // the shape of the Apex TakeHomePayWrapper class
+        const takeHomePayParameters = {
+            annualIncome: this.annualIncome,
+            filingStatus: this.filingStatus,
+            taxableIncome: this.taxableIncome
+        };
+        // Calculate Taxes and Paycheck
+        try {
+            const calcResults = await getTaxesAndPayChecks({ wrapper: takeHomePayParameters });
+            this.error = undefined;
+            this.federalTaxValue = calcResults['federalTaxValue'];
+            this.socialSecurity = calcResults['socialSecurity'];
+            this.medicare = calcResults['medicare'];
+        } catch (error) {
+            this.federalTaxValue = 0;
+            this.socialSecurity = 0;
+            this.medicare = 0;
+            this.error = error;
         }
     }
 }
