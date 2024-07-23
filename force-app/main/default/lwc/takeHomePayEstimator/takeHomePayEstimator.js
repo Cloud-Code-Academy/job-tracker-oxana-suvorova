43637090d34b0ae2695b39cc358bd22e4524e5af
Oxana <mail.suvorova.o@gmail.com>
2024-07-22 22:40:42 -0400

    JAT-37 Finished calculations. Add recordId, requested fields and update Job App record

--- force-app/main/default/lwc/takeHomePayEstimator/takeHomePayEstimator.js
+++ force-app/main/default/lwc/takeHomePayEstimator/takeHomePayEstimator.js
@@ -1,15 +1,37 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc';
 import getTaxDeduction from '@salesforce/apex/TakeHomePayEstimatorController.getTaxDeduction';
 import getTaxesAndPayChecks from '@salesforce/apex/TakeHomePayEstimatorController.getTaxesAndPayChecks';
+import updateJobApplication from '@salesforce/apex/TakeHomePayEstimatorController.updateJobApplication';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+import { getRecord, getFieldValue, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
+import SALARY_FIELD from '@salesforce/schema/Job_Application__c.Salary__c';
+
+const fields = [SALARY_FIELD];
 
 export default class TakeHomePayEstimator extends LightningElement {
+    @api recordId;
     annualIncome = 100000;
+    recordPage = false;
+
+    @wire(getRecord, { recordId: '$recordId', fields: fields })
+    wiredRecord({ error, data }) {
+        if (data) {
+            this.annualIncome = getFieldValue(data, SALARY_FIELD);
+            this.error = undefined;
+            this.recordPage = true;
+        } else if (error) {
+            this.error = error.body.message;
+        }
+    }
+
     filingStatus = 'Single';
     standardDeduction = 0;
     federalTaxValue = 0;
     socialSecurity = 0;
     medicare = 0;
 
+    @track needCalculate = true;
     @track error;
 
     get taxableIncome() {
@@ -31,24 +53,25 @@ export default class TakeHomePayEstimator extends LightningElement {
         { label: 'Head of household', value: 'HeadOfHousehold' }
     ];
 
+    @wire(getTaxDeduction, { filingStatus: '$filingStatus' } )
+    setStandardDeduction({ error, data }) {
+        if (data) {
+            this.standardDeduction = data;
+            this.error = undefined;
+            this.needCalculate = true;
+        } else if (error) {
+            this.error = error.body.message;
+        }
+    };
+
     annualIncomeChange(event) {
         this.annualIncome = event.target.value;
+        this.needCalculate = true;
     }
 
     filingStatusChange(event) {
         this.filingStatus = event.target.value;
     }
-
-    @wire(getTaxDeduction, { filingStatus: '$filingStatus', isFederal: true } )
-    setStandardDeduction({ error, data }) {
-        if (data) {
-            this.standardDeduction = data;
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-        }
-        
-    };
 
     async handleEstimateClick() {
         // Create the object with parameters with
@@ -66,10 +89,42 @@ export default class TakeHomePayEstimator extends LightningElement {
             this.socialSecurity = calcResults['socialSecurity'];
             this.medicare = calcResults['medicare'];
         } catch (error) {
-            this.federalTaxValue = 0;
-            this.socialSecurity = 0;
-            this.medicare = 0;
-            this.error = error;
+            this.error = error.body.message;
+        }
+        this.needCalculate = false;
+    }
+
+    async handleUpdateJobAppClick() {
+        // Create the object with parameters with
+        // the shape of the Apex TakeHomePayWrapper class
+        const takeHomePayResults = {
+            federalTaxValue: this.federalTaxValue,
+            socialSecurity: this.socialSecurity,
+            medicare: this.medicare,
+            annualPaycheck: this.annualPaycheck,
+            monthlyPaycheck: this.monthlyPaycheck
+        };
+        try {
+            await updateJobApplication({
+                recordId: this.recordId,
+                wrapper: takeHomePayResults
+            });
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Success',
+                    message: 'Job Application record updated',
+                    variant: 'success'
+                })
+            );
+            notifyRecordUpdateAvailable([{ recordId: this.recordId }]);
+        } catch (error) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error updating record',
+                    message: error.body.message,
+                    variant: 'error'
+                })
+            );
         }
     }
 }
